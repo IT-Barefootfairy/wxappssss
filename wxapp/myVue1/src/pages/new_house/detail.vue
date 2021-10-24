3d2c82071871febde2a667708d86b041a97357d9
gaojian <3107833744@qq.com>
2021-10-24 19:54:33 +0800

    aaa

--- wxapp/myVue1/src/pages/new_house/detail.vue
+++ wxapp/myVue1/src/pages/new_house/detail.vue
@@ -19,6 +19,38 @@
       </view>
       <view class="xq">详情 ></view>
     </view>
+
+    <view class="message">
+      <view class="jun">
+        <view class="num">
+          <view class="num_n">{{build_price_jun}}</view>
+          <view class="yuan">元/m²</view>
+        </view>
+        <view class="text">参考均价
+          <image class="img" style="width:12px;height:12px;" src="../../static/prompt.png" alt=""/>
+        </view>
+      </view>
+      <view class="zong">
+        <view class="num">
+          <view class="num_n">{{build_price_zong}}</view>
+          <view class="yuan">起</view>
+        </view>
+        <view class="text">
+          <view class="text">参考总价
+          <image class="img" style="width:12px;height:12px;" src="../../static/prompt.png" alt=""/>
+        </view>
+        </view>
+      </view>
+      <view class="mj">
+        <view class="num">
+          <view class="num_n">{{build_square}}</view>
+          <view class="yuan">m²</view>
+        </view>
+        <view class="text">
+          <view class="text">建面范围</view>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -36,6 +68,9 @@ export default {
       buildImgCount: 0,
       build: {},
       build_type: [],
+      build_price_jun:0,
+      build_price_zong:"",
+      build_square:""
     });
 
     const datas = toRefs(data);
@@ -44,11 +79,14 @@ export default {
       let { id } = Taro.getCurrentInstance().router.params;
       data.id = Number(id);
       gethotDetail(data.id).then((res) => {
-        console.log(res, "3----------");
+        // console.log(res, "3----------");
         data.headImage = res.focus;
         data.buildImgCount = res.buildImgCount;
         data.build = res.build;
         data.build_type = res.build.build_type;
+        data.build_price_jun=res.build.build_price;
+        data.build_price_zong=res.build.reference_price;
+        data.build_square=res.build.main_huxing;
       });
     });
 
