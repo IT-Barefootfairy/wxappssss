0e3e152ed6d891affed477e69c70c91ebc04a3c6
gaojian <3107833744@qq.com>
2021-10-28 18:58:41 +0800

    ds

--- wxapp/egg-myapp/app/router.js
+++ wxapp/egg-myapp/app/router.js
@@ -4,21 +4,14 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app;
-  router.get('/', controller.home.index);
-  router.get('/mypay',controller.home.mypay);
-  router.get('/list',controller.home.list);
-  router.get('/suqianhome', controller.home.suqianHome)
-  // 登录接口
-  router.get('/login',controller.home.login)
-  router.get('/returhtml', controller.home.returhtml)
-  // 首页接口
-  router.get('/customindex', controller.sqzf.customindex)
-  // 找房接口
-  router.get('/customhose', controller.sqzf.customhose)
-  router.get('/customhoseList', controller.sqzf.customhoseList)
-  router.get('/customershou',controller.sqzf.customershou)
-  router.get('/chatFriends',controller.sqzf.chatFriends)
-  router.get('/fangFriends',controller.sqzf.fangFriends)
-  router.get('/messageDetail',controller.sqzf.messageDetail)
+    const { router, controller } = app;
+    router.get('/', controller.home.index);
+    router.get('/mypay', controller.home.mypay);
+    router.get('/list', controller.home.list);
+    router.get('/suqianhome', controller.home.suqianHome);
+    router.get('/login', controller.home.login);
+    router.get('/returhtml', controller.home.returhtml);
+    router.get('/customindex', controller.sqzf.customindex);
+    router.get('/ershou', controller.sqzf.customershou);
+    router.get('/hotbuildetail', controller.sqzf.hotBuildDetail);
 };
